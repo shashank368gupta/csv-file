1eecaf7ac4a4849c7fc71fbd946c8ea123f3d9d1
Shashank gupta <shashank.gupta@reddoorz.com>
2023-02-26 12:22:00 +0530

    built out navigation and welcome controller and set root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'welcome/about'
+  get 'welcome/contact'
+  get 'welcome/faq'
+  get 'welcome/pricing'
+  get 'welcome/features'
   resources :invoices
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  root to: "welcome#index"
 end
